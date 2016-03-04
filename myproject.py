9338b3145f0a21f98d9a948417bc670bbd34d41b
lukekearney <luke.t.kearney@gmail.com>
2016-03-04 12:16:23 +0000

    Added new routes. Different project structure

--- myproject.py
+++ myproject.py
@@ -1,9 +1,14 @@
 from flask import Flask
+import src
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
     return "<h1 style='color:blue'>Hello There!</h1>"
+
+@application.route("/about")
+def about():
+    return "BLUH"
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0')
---
+++ src/__init__.py
@@ -0,0 +1,3 @@
+# Practical 19
+# Luke Kearney
+# 15205591
