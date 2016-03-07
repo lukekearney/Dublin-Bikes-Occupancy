be7f1f8f05520fad1741245fdd0b381cb345e512
Conor Sheehan <conor.sheehan@live.ie>
2016-03-07 15:23:41 +0000

    add json files and endpoint code /end

--- frontend/front.py
+++ frontend/front.py
@@ -1,1 +1,21 @@
-print("hello")
+import json
+from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class HelloWorld(Resource):
+    def get(self):
+        with open('static/first_snapshot.json') as data_file:
+            data = json.load(data_file)
+        return data
+
+api.add_resource(HelloWorld, '/end')
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run(host='localhost')
+    
+#add /end to url to return json
