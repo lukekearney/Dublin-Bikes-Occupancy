cd19669fe466dd0452291728d7a45031d16d534f
Luke Kearney <luke.t.kearney@gmail.com>
2016-03-19 17:30:11 +0000

    Added sample database file and log

--- server.py
+++ server.py
@@ -1,7 +1,13 @@
 from flask import Flask, request, session, g, redirect, url_for, \
      abort, render_template, flash
 
+import sqlite3, os
 import src
+
+path = os.path.dirname(os.path.realpath(__file__))
+conn = sqlite3.connect(path + "/bikes.db")
+
+c = conn.cursor()
 application = Flask(__name__)
 
 @application.route("/")
@@ -21,6 +27,13 @@ def api():
         return render_template('404.html'), 404
     else:
         return racknum
+
+# Add whatever api route you want here
+@application.route('/api/static')
+def static_api():
+
+
+    
     
 
 if __name__ == "__main__":
