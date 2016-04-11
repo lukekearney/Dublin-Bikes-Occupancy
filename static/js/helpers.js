283df867e11fa058c17d41ad0fe4f1283260cd6b
Luke Kearney <luke.t.kearney@gmail.com>
2016-04-11 13:39:11 +0100

    Marker colours

--- static/js/helpers.js
+++ static/js/helpers.js
@@ -1,11 +1,12 @@
 function getColour(decimal) {
 	var dec = parseInt("ffffff", 16);
-	var colour = Math.round(dec * decimal)
+	var colour = Math.round(255 * decimal)
 	
 	var hex = colour.toString(16);
 	
-	return "rgb(" + Math.round(255 * decimal)+ ", 80, 80)";
-	//return "#" + hex;
+	return "rgb(255, " + colour + ", 0)";
+	//console.log(hex);
+	//return "#ff" + hex + "00";
 }
 
 // http://stackoverflow.com/questions/3710204/how-to-check-if-a-string-is-a-valid-json-string-in-javascript-without-using-try/3710226
