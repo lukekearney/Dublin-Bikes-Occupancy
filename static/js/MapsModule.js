ce604c1b0e5d3eff18a96c7456b23b85d6877d45
Luke Kearney <luke.t.kearney@gmail.com>
2016-04-15 11:13:42 +0100

    merging

---
+++ static/js/MapsModule.js
@@ -0,0 +1,116 @@
+// set up IIFE, only exposing the parts we want to be globally accessible
+// set up maps module
+var MapsModule = (function(){
+	// allow referencing of the entire maps module
+	var self = this;
+
+	// private object property, available to the MapsModule
+	var settings = {
+		"map": null,
+		"markers": []
+	}
+
+	// private method
+	function setUpMap(){
+		
+		var map;
+
+		// insert the google map object
+	    map = new google.maps.Map(settings.map, {
+			center: {lat: 53.3474000259898, lng: -6.259202954653347},
+			zoom: 13
+        });
+
+        settings.mapObj = map;
+
+	}
+
+	function getMapData(callback){
+		var request = window.superagent;
+		var url = "http://localhost:5000/api/static";
+		request.get(url, function(err, response){
+			// console.log('Response ok:', response.ok);
+			// console.log('Response text:', response.text);
+			// need to do more error handling here.
+			if (!err){
+				var data = JSON.parse(response.text);
+				for (var i = 0; i < data.length; i++){
+					console.log(data[i]);
+					var marker = new google.maps.Marker({
+						position: {
+							"lat": data[i].position.lat,
+							"lng": data[i].position.lang
+						},
+						map: settings.mapObj,
+						title: data[i].name,
+						number: data[i].number
+					});
+
+					marker.addListener("click", function(){
+						// fetches data based on the marker's number
+						BikesModule.getStationHistoricalInformation(this.number, 0)
+					});
+
+					settings.markers.push(marker)
+				}
+			}
+		});
+		//based on http://www.w3schools.com/ajax/tryit.asp?filename=tryajax_get
+		// var xmlhttp = new XMLHttpRequest();
+		// var url = "https://localhost:5000/api/static";
+		
+
+		// xmlhttp.onreadystatechange = function () {
+		// 	if (xmlhttp.readyState === 4 && xmlhttp.status == 200) {
+		// 		// success
+		// 		// check if callback function is defined
+		// 		if (callback){
+		// 			// run callback
+		// 			callback(xmlhttp.response);
+		// 		} else {
+		// 			// default response to successful request
+		// 			console.log("Request completed successfully: ");
+		// 			console.log(response);
+					
+		// 		}
+		// 	} else if (xmlhttp.readyState === 4 && (xmlhttp.status >= 400 || xmlhttp.status < 500)){
+		// 		// error handling
+		// 		console.log("uh oh");
+		// 		console.error(xmlhttp.statusText);
+		// 	}
+		// }
+		// xmlhttp.open("GET", url, true);
+
+		// xmlhttp.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+		// xmlhttp.setRequestHeader("Accept-Language", "en-US,en;q=0.8");
+		// xmlhttp.setRequestHeader("Pragma", "no-cache");
+
+		// xmlhttp.send();
+
+	}
+
+	// public exposed properties
+	return {
+		init: function(element){
+			settings.map = element;
+			// set up the map
+			setUpMap();
+
+			// call getMapData with the callback function
+			getMapData(function(resp){
+				console.log(resp);
+			});
+		},
+
+		addMarkers: function(json){
+			// add each marker to the map
+			/*
+			for each item in json
+				add marker to the map
+				add info box
+				add marker listeners for charted data
+			end for
+			*/
+		}
+	}
+}())
---
+++ static/js/jasmine/spec/BikesSpec.js
@@ -0,0 +1,83 @@
+describe("Fetch Station info", function() {
+	var res;
+	beforeEach(function(done) {
+	   BikesModule.getStationInfo("north-circular-road", function(err, result) {
+	   	res = result;
+	   	done();
+	   })
+	  });
+	
+
+    it("should fetch a numerical station id as part of the result", function(done) {
+	    expect(res[0]["number"] > 0 && res[0]["number"] < 101).toBeTruthy();
+	    done();
+	    
+	});
+});
+
+describe("Fetch Station historical data", function() {
+	var res;
+	var errors = 0;
+	beforeEach(function(done) {
+	    BikesModule.getStationHistoricalInformation(49, 0, function(err, result) {
+
+	   	for (var i = 0; i < result.length; i++) {
+	   		if (result[i].day != 0 || result[i].number != 49) {
+	   			
+	   			errors++;
+	   		} 
+	   	}
+	   	done();
+	   })
+	  });
+	
+
+    it("should fetch an array of objects giving occupancy data for the given day and station", function(done) {
+	    expect(errors === 0).toBeTruthy();
+	    done();
+	    
+	});
+});
+
+describe("Fetch Station historical data by number only", function() {
+	var res;
+	var valid = true;
+	beforeEach(function(done) {
+	   BikesModule.getStationHistoricalInformation(49, null, function(err, result) {
+	   	res = result;
+	   	for (var i = 0; i < result.length; i++) {
+	   		if (result[i].day > 6 || result[i].day < 0 || result[i].number != 49) {
+	   			valid = false;
+	   		}
+	   	}
+	   	done();
+	   })
+	  });
+	
+
+    it("should fetch an array of objects giving occupancy data for the given station", function(done) {
+	    expect(valid).toBeTruthy();
+	    done();
+	    
+	});
+});
+
+describe("Fetch All real time data", function() {
+	var res;
+	var valid = true;
+	beforeEach(function(done) {
+	    BikesModule.getRealTimeData(49, null, function(err, result) {
+		   	if (result.length == 0) {
+		   		valid = false;
+		   	}
+	   	done();
+	   })
+	  });
+	
+
+    it("should fetch an array of objects giving occupancy data for the given station", function(done) {
+	    expect(valid).toBeTruthy();
+	    done();
+	    
+	});
+});
